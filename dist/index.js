d6c251b87364388242bbcb0318e900d8e9064302
Troy Alford <troy@troyalford.com>
2017-01-29 15:38:51 -0800

    Updating dist

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.NODE_TYPES = undefined;
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
@@ -26,7 +27,10 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var NODE_TYPE = {
+var NODE_TYPES = exports.NODE_TYPES = {
+  ELEMENT: 1,
+  TEXT: 3,
+
   1: 'Element',
   3: 'Text',
   7: 'Processing Instruction',
@@ -43,11 +47,29 @@ var NODE_TYPE = {
   12: 'XML Notation (Deprecated)'
 };
 
-var Node = {
-  Element: 1,
-  Text: 3
+var ATTRIBUTES = {
+  'class': 'className',
+  'for': 'htmlFor'
 };
+
 var parser = new DOMParser();
+
+var warnParseErrors = function warnParseErrors(doc) {
+  var errors = Array.from(doc.documentElement.childNodes);
+  console.warn('Unable to parse jsx. Found ' + errors.length + ' error(s):');
+
+  var warn = function warn(node, indent) {
+    if (node.childNodes.length) Array.from(node.childNodes).forEach(function (n) {
+      return warn(n, indent.concat(' '));
+    });
+
+    console.warn(indent + '==> ' + node.nodeValue);
+  };
+
+  errors.forEach(function (e) {
+    return warn(e, ' ');
+  });
+};
 
 var JsxParser = function (_Component) {
   _inherits(JsxParser, _Component);
@@ -71,21 +93,32 @@ var JsxParser = function (_Component) {
     }
   }, {
     key: 'parseJSX',
-    value: function parseJSX(jsx) {
-      if (!jsx) return [];
+    value: function parseJSX(rawJSX) {
+      if (!rawJSX || typeof rawJSX !== 'string') return [];
 
-      var wrapped = '<!DOCTYPE html><html><body>' + jsx + '</body></html>';
-      var doc = parser.parseFromString(wrapped, 'text/html');
+      var jsx = this.props.blacklistedTags.reduce(function (raw, tag) {
+        return raw.replace(new RegExp('(</?)' + tag, 'ig'), '$1REMOVE');
+      }, rawJSX);
+
+      var wrapped = '\n      <?xml version="1.0" encoding="UTF-8"?>      <xml>' + jsx + '</xml>    ';
+      var doc = parser.parseFromString(wrapped, 'application/xml');
       if (!doc) return [];
 
-      var body = doc.getElementsByTagName('body')[0];
-      if (!body || body.nodeName.toLowerCase() === 'parseerror') return [];
+      Array.from(doc.getElementsByTagName('REMOVE')).forEach(function (tag) {
+        return tag.parentNode.removeChild(tag);
+      });
+
+      var xml = doc.getElementsByTagName('xml')[0];
+      if (!xml || xml.nodeName.toLowerCase() === 'parseerror') {
+        warnParseErrors(doc);
+        return [];
+      }
 
       var components = this.props.components.reduce(function (map, type) {
-        return _extends({}, map, _defineProperty({}, type.constructor.name, type));
+        return _extends({}, map, _defineProperty({}, type.prototype.constructor.name, type));
       }, {});
 
-      return this.parseNode(doc.body.childNodes || [], components);
+      return this.parseNode(xml.childNodes || [], components);
     }
   }, {
     key: 'parseNode',
@@ -95,24 +128,27 @@ var JsxParser = function (_Component) {
       var components = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
       var key = arguments[2];
 
-      if (node instanceof NodeList || Array.isArray(node)) return Array.from(node) // handle nodeList or []
-      .map(function (child, key) {
-        return _this2.parseNode(child, components, key);
-      }).filter(function (child) {
-        return child;
-      }); // remove falsy nodes
+      if (node instanceof NodeList || Array.isArray(node)) {
+        return Array.from(node) // handle nodeList or []
+        .map(function (child, key) {
+          return _this2.parseNode(child, components, key);
+        }).filter(function (child) {
+          return child;
+        }); // remove falsy nodes
+      }
 
       switch (node.nodeType) {
-        case Node.Text:
+        case NODE_TYPES.TEXT:
           // Text node. Collapse whitespace and return it as a String.
-          return ('textContent' in node ? node.textContent : node.nodeValue || '').replace(/\w/g, ' ').replace(/  /g, ' ').trim();
+          return ('textContent' in node ? node.textContent : node.nodeValue || '').replace(/\s{2,}/g, ' ').trim();
 
-        case Node.Element:
+        case NODE_TYPES.ELEMENT:
           // Element node. Parse its Attributes and Children, then call createElement
           return _react2.default.createElement(components[node.nodeName] || node.nodeName, this.parseAttrs(node.attributes, key), this.parseNode(node.childNodes, components));
 
         default:
-          console.warn('JsxParser encountered a ' + NODE_TYPE[node.nodeType] + ' node, and discarded it.');
+          console.warn('JsxParser encountered a(n) ' + NODE_TYPES[node.nodeType] + ' node, and discarded it.');
+          return null;
       }
     }
   }, {
@@ -121,15 +157,27 @@ var JsxParser = function (_Component) {
       var props = { key: key };
       if (!attrs || !attrs.length) return props;
 
-      return Array.from(attrs).reduce(function (current, attr) {
+      var blacklist = this.props.blacklistedAttrs;
+
+      return Array.from(attrs).filter(function (attr) {
+        return !blacklist.map(function (mask) {
+          return (
+            // If any mask matches, it will return a non-null value
+            attr.name.match(new RegExp(mask, 'gi'))
+          );
+        }).filter(function (match) {
+          return match !== null;
+        }).length;
+      }).reduce(function (current, attr) {
         var name = attr.name,
             value = attr.value;
 
         if (value === '') value = true;
         if (name.substring(0, 2) === 'on') value = new Function(value); // eslint-disable-line no-new-func
-        if (name === 'class') name = 'className';
 
-        return _extends({}, current, _defineProperty({}, (0, _camelCase2.default)(name), value));
+        name = ATTRIBUTES[name.toLowerCase()] || (0, _camelCase2.default)(name);
+
+        return _extends({}, current, _defineProperty({}, name, value));
       }, props);
     }
   }, {
@@ -150,10 +198,25 @@ exports.default = JsxParser;
 
 
 JsxParser.propTypes = {
-  components: _react2.default.PropTypes.arrayOf(_react2.default.PropTypes.oneOf([_react2.default.PropTypes.instanceOf(_react2.default.Component), _react2.default.PropTypes.instanceOf(_react2.default.PureComponent)])),
+  allowScripts: _react2.default.PropTypes.bool,
+  blacklistedAttrs: _react2.default.PropTypes.arrayOf(_react2.default.PropTypes.string),
+  blacklistedTags: _react2.default.PropTypes.arrayOf(_react2.default.PropTypes.string),
+  components: function components(props, propName, componentName) {
+    if (!Array.isArray(props[propName])) return new Error(propName + ' must be an Array of Components.');
+
+    var passes = true;
+    props[propName].forEach(function (component) {
+      if (!component.prototype instanceof _react2.default.Component && !component.prototype instanceof _react2.default.PureComponent) passes = false;
+    });
+
+    return passes ? null : new Error(propName + ' must contain only Subclasses of React.Component or React.PureComponent.');
+  },
   jsx: _react2.default.PropTypes.string
 };
 JsxParser.defaultProps = {
+  allowScripts: false,
+  blacklistedAttrs: ['on[a-z]*'],
+  blacklistedTags: ['script'],
   components: [],
   jsx: ''
 };
