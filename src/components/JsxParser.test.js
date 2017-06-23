071d98e581c2815f59f8615d506076c80bd5524f
Troy Alford <troy@troyalford.com>
2017-06-23 16:03:17 -0700

    Removing react-addons-test-utils in favor of react-dom/test-utils

--- src/components/JsxParser.test.js
+++ src/components/JsxParser.test.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import TestUtils from 'react-addons-test-utils'
+import TestUtils from 'react-dom/test-utils'
 import JsxParser from './JsxParser'
 
 jest.unmock('./JsxParser')
