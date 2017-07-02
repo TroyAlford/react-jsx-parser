e7c8a80127c0d7345217d55e4ceeb2e685d87031
Troy Alford <TroyAlford@users.noreply.github.com>
2017-07-01 19:21:23 -0700

    v1.1.0 (#21)
    * Adding better badges & links
    * AcornJSX Refactor
    * Parsing fixes for object/array based props, and props surrounded with `{}` instead of `""`
    * Fixed issue with implicit boolean props, such as `readOnly`

--- source/helpers/hasDoctype.js
+++
@@ -1,7 +0,0 @@
-export default function hasDoctype(jsx) {
-  return /<(\/?|!?)DOCTYPE html>/.test(jsx) &&
-    /<(\/?|!?)html>/.test(jsx) &&
-    /<(\/?|!?)head>/.test(jsx) &&
-    /<(\/?|!?)body>/.test(jsx)
-}
-
