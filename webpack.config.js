cfc146e76fc0bd9e6ac024f6571ef9ca2ec53287
Troy Alford <troy@troyalford.com>
2018-06-03 15:53:36 -0700

    Updating dependencies & migrating to webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 const path = require('path')
 const webpack = require('webpack')
 
-const ENVIRONMENT = process.env.NODE_ENV
+const ENVIRONMENT = process.env.NODE_ENV || 'development'
 const PRODUCTION = ENVIRONMENT === 'production'
 const SOURCEMAP = !PRODUCTION || process.env.SOURCEMAP
 
@@ -13,37 +13,34 @@ const plugins = []
 
 if (PRODUCTION) {
   plugins.push(
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(ENVIRONMENT),
-    }),
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify(ENVIRONMENT) }),
     new webpack.optimize.ModuleConcatenationPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      output: { comments: false, semicolons: false },
-      sourceMap: SOURCEMAP,
-    })
   )
 }
 
 module.exports = {
   devtool: SOURCEMAP ? 'source-map' : 'none',
-  entry:  `${__dirname}/source/components/JsxParser.js`,
+  entry: `${__dirname}/source/components/JsxParser.js`,
   externals: {
     'react': 'react',
     'react-dom': 'react-dom',
   },
+  mode: ENVIRONMENT,
   module: {
-    loaders: [{
-      test:    /\.js$/,
-      loader:  'babel-loader',
+    rules: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
       exclude: /node_modules/,
     }],
+  },
+  optimization: {
+    minimize: PRODUCTION,
   },
   output: {
     filename,
     library,
-    path:           `${__dirname}/lib`,
-    libraryTarget:  'umd',
+    path: `${__dirname}/lib`,
+    libraryTarget: 'umd',
     umdNamedDefine: true,
   },
   plugins,
