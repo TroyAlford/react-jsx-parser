e2d91e60ecb670c3d3b3fd2b34424667d673e77e
Troy Alford <TroyAlford@users.noreply.github.com>
2018-11-13 10:26:07 -0500

    Update tests to include unary NOT operations
    Co-Authored-By: hokevins <ho.kevins@gmail.com>

--- source/components/JsxParser.test.js
+++ source/components/JsxParser.test.js
@@ -708,6 +708,11 @@ describe('JsxParser Component', () => {
       expect(rendered.childNodes[0].textContent).toEqual('-75')
       expect(component.ParsedChildren[0].props.testProp).toEqual(-60)
     })
+    it('can execute unary NOT operations', () => {
+      const { rendered, component } = render(<JsxParser jsx={'<span testProp={!60}>{ !false }</span>'} />)
+      expect(rendered.childNodes[0].textContent).toEqual('true')
+      expect(component.ParsedChildren[0].props.testProp).toEqual(false)
+    })
   })
   describe('React.Children.only()', () => {
     // eslint-disable-next-line react/prop-types
