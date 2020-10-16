73db816f9e74b990008bc24943c498589d5bf295
Troy Alford <talford@vestmark.com>
2020-10-16 16:33:12 -0700

    Update dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,12 @@
 module.exports = {
 	plugins: [
-		['@babel/plugin-transform-typescript', { allowDeclareFields: true }],
+		'@babel/plugin-transform-typescript',
 		'@babel/plugin-proposal-class-properties',
 	],
 	presets: [
-		['@babel/preset-env', { corejs: '3.6.5', modules: 'auto', useBuiltIns: 'entry' }],
+		'@babel/preset-env',
 		'@babel/preset-react',
-		['@babel/preset-typescript', { allowDeclareFields: true }],
+		'@babel/preset-typescript',
 	],
 	highlightCode: true,
 }
