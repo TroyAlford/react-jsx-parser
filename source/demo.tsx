d0eede5e63aba7ba5795045b20f6e5a3c1518ea2
Troy Alford <TroyAlford@users.noreply.github.com>
2024-06-30 20:24:28 -0500

    update to react@18 and @cfaester/enzyme-adapter-react-18

--- source/demo.tsx
+++ source/demo.tsx
@@ -1,18 +1,19 @@
 /* eslint-disable no-console */
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 // @ts-expect-error - untyped output file
 import JsxParser from '../dist/umd/react-jsx-parser.min'
 
-ReactDOM.render(
+const root = createRoot(document.querySelector('#root')!)
+
+root.render(
 	<JsxParser
 		autoCloseVoidElements
 		jsx={`
-			<img src="http://placekitten.com/300/500">
+			<img src="https://picsum.photos/300/500">
 			<div className="foo">bar</div>
 		`}
 		onError={console.error}
 		showWarnings
 	/>,
-	document.querySelector('#root'),
 )
